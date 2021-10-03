2e53114bf790964f27e23155b2536bc17edb6e54
mohammadmolavi <mohammadmolavi97@gmail.com>
2021-10-03 22:37:04 +0330

    data convert to object

--- project01.cpp
+++ project01.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 #include <string.h>
 #include <fstream>
 #include <vector>
-
+#include <list>
 using namespace std;
 
 class detailes
@@ -16,25 +17,25 @@ private:
     string types;
     string attributes;
     bool isOriginalTitle;
+
 public:
-    detailes(string = "" , int = 0, string = "" , string = "" , string = ""
-     , string = "" , string = "" , bool = false);
-    void set_titleId(string);
-    string get_titleId();
-    void set_ordering(string);
-    int get_ordering();
-    void set_title(string);
-    string get_title();
-    void set_region(string);
-    string get_region();
-    void set_language(string);
-    string get_language();
-    void set_types(string);
-    string get_types();
-    void set_attributes(string);
-    string get_attributes();
-    void set_isOriginalTitle(string);
-    bool get_isOriginalTitle();
+    //detailes(string = "", int = 0, string = "", string = "", string = "", string = "", string = "", bool = false);
+    void set_titleId(string tId) { titleId = tId; };
+    string get_titleId() { return titleId; };
+    void set_ordering(string order) { ordering = stoi(order); };
+    int get_ordering() { return ordering; };
+    void set_title(string titl) { title = titl; };
+    string get_title() { return title; };
+    void set_region(string reg) { region = reg; };
+    string get_region() { return region; };
+    void set_language(string lang) { language = lang; };
+    string get_language() { return language; };
+    void set_types(string type) { types = type; };
+    string get_types() { return types; };
+    void set_attributes(string att) { attributes = att; };
+    string get_attributes() { return attributes; };
+    void set_isOriginalTitle(char originalTitle) { isOriginalTitle = (int)originalTitle - 48; };
+    bool get_isOriginalTitle() { return isOriginalTitle; };
 };
 
 class rating
@@ -43,8 +44,51 @@ private:
     string titleId;
     int count;
     float rate;
+
 public:
-    rating(string , int , float);
-    void new_rate(bool);
-    float get_rating();
+    rating(string, int, float);
+    void new_rate(int);
+    float get_rating() { return rate; };
 };
+
+void rating::new_rate(int rat)
+{
+    rate *= 1.0 * count;
+    rate += rat;
+    rate /= 1.0 * count;
+}
+
+int main()
+{
+    ifstream inputFile("title.akas.txt", ios::in);
+    string line;
+    getline(inputFile, line);
+    vector <detailes> list;
+    while (line[0] = '0')
+    {
+        detailes mov;
+        getline(inputFile, line);
+        char *str = &line[0];
+        char *token = (char *)strtok(str, "\t");
+        if (token[0] == '0')
+            break;
+        mov.set_titleId(token);
+        token = strtok(NULL, "\t");
+        mov.set_ordering(token);
+        token = strtok(NULL, "\t");
+        mov.set_title(token);
+        token = strtok(NULL, "\t");
+        mov.set_region(token);
+        token = strtok(NULL, "\t");
+        mov.set_language(token);
+        token = strtok(NULL, "\t");
+        mov.set_types(token);
+        token = strtok(NULL, "\t");
+        mov.set_attributes(token);
+        token = strtok(NULL, "\t");
+        mov.set_isOriginalTitle(token[0]);
+        token = strtok(NULL, "\t");
+        list.push_back(mov);
+    }
+    inputFile.close();
+}
